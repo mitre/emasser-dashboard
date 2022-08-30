17decd1f7ed0b76fdc2c958dacc3a09a78778f09
George Dias <george.m.dias.ctr@army.mil>
2022-08-30 12:47:08 -0500

    Fixed emass_client registry location

--- pages/get/systems.vue
+++ pages/get/systems.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div>
+  <div class='h-screen'>
     <div class='vld-parent'>
       <loading :active.sync='isLoading' 
         :can-cancel='true' 
@@ -127,7 +127,7 @@
 
       <!-- OUtput Content -->
       <div
-        class='font-normal rounded-2xl mt-4 md:px-8 py-2 pb-6'
+        class='font-normal rounded-2xl mt-4 md:px-8 py-2 pb-6 h-96'
         style='background-color: rgb(146 151 179 / 13%)'
       >
         <div>
@@ -137,11 +137,11 @@
           <vue-table-dynamic :params="params"></vue-table-dynamic>
         </div> -->
 
-      <div class="overflow-scroll flex flex-col flex-wrap mt-2 pl-2 h-max">
+      <div class="overflow-scroll flex flex-col flex-wrap mt-2 pl-2">
         <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
             <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table class='min-w-full divide-y divide-gray-300'>
+                <table class='min-w-full divide-y divide-gray-300 h-96'>
                   <thead class='bg-gray-50 '>
                     <tr>
                       <th scope='col' class='py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6'>System Id</th>
